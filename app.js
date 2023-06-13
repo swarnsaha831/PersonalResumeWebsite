65ad0c944c88a027ce024667ed154d72dc42e23c
swarnsaha831 <136046033+swarnsaha831@users.noreply.github.com>
2023-06-13 00:45:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
     // Smooth scrolling
     $("a").on("click", function(event) {
-      if (this.hash !== "") {
-        event.preventDefault();
         var hash = this.hash;
-        var offset = $(hash).offset().top;
-        var navbarHeight = $(".navbar").outerHeight();
-        $("html, body").animate({
-          scrollTop: offset - navbarHeight
-        }, 800, function() {
-          // Update the URL without adding the hash to the end
-          history.replaceState(null, null, hash);
-        });
-      }
+        // Special case for the 'Swarnadeep Saha' link
+        if (this.text === "Swarnadeep Saha") {
+            hash = "body";
+        }
+        if (hash !== "") {
+            event.preventDefault();
+            var offset = $(hash).offset().top;
+            var navbarHeight = $(".navbar").outerHeight();
+            $("html, body").animate({
+                scrollTop: offset - navbarHeight
+            }, 800, function() {
+                if (this.text !== "Swarnadeep Saha") {
+                    // Update the URL without adding the hash to the end
+                    history.replaceState(null, null, hash);
+                }
+            }.bind(this)); // Bind 'this' to have access to it inside the callback
+        }
     });
 });
